0a3ea34072559f1ba6849977e63ee57dceb899dd
ian-ofgod <ian.didio@mail.polimi.it>
2021-02-20 10:45:01 +0100

    Added LoginLog

--- src/main/java/it/polimi/db2/application/entities/User.java
+++ src/main/java/it/polimi/db2/application/entities/User.java
@@ -23,16 +23,15 @@ public class User implements Serializable {
 	private Boolean banned;
 
 	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
-	private List<Login_Log> logs;
+	private List<LoginLog> logs;
 
 	//EMPTY
 	public User() {
 	}
 
 	//METHODS
-	public void addNewLog(Login_Log log){
+	public void addNewLog(LoginLog log){
 		logs.add(log);
-		log.setUser(this);
 	}
 
 
@@ -77,11 +76,11 @@ public class User implements Serializable {
 		this.banned = banned;
 	}
 
-	public List<Login_Log> getLogs() {
+	public List<LoginLog> getLogs() {
 		return logs;
 	}
 
-	public void setLogs(List<Login_Log> logs) {
+	public void setLogs(List<LoginLog> logs) {
 		this.logs = logs;
 	}
 }
